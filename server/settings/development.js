d2313204b108161dfedd1f468a4de79484b1a4ad
SebastienLouis <sebastien.louis.pro@gmail.com>
2016-02-05 12:35:36 +0100

    add commentary

--- server/settings/development.js
+++ server/settings/development.js
@@ -1,10 +1,17 @@
 module.exports = {
     port: process.env.PORT || 8080,
-    //LOCALHOST
-    db: 'mongodb://heroku_0bcx23jj:9m6811rb7vodv4pkrrapuo7aab@ds059185.mongolab.com:59185/heroku_0bcx23jj',
-    sessionDb: 'mongodb://admin:admin@ds055895.mongolab.com:55895/heroku_z34shwgt',
 
-    //db: 'mongodb://inovea_eventdb',
-    //sessionDb:'mongodb://inovea:inovea4iam@ds055485.mongolab.com:55485/inovea_eventdb',
+    /***********
+     * LOCALHOST
+     ************/
+    //db: 'mongodb://localhost:27017/todo-list-api',
+    //sessionDb: 'mongodb://localhost:27017/todo-list-session-db',
+
+    /***********
+     * MONGOLAB
+     ************/
+    db: 'mongodb://heroku_0bcx23jj:9m6811rb7vodv4pkrrapuo7aab@ds059185.mongolab.com:59185/heroku_0bcx23jj',
+    sessionDb: 'mongodb://heroku_z34shwgt:ed7ur8153k398p2bqs96hieor2@ds055895.mongolab.com:55895/heroku_z34shwgt',
+
     sessionTTL: 2 * 24 * 60 * 60
 };
