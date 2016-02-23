545c88e423e37a3ff339bf11e1e7ac3a7cd78651
Oussama Bentalha <oussama.bentalha@gmail.com>
2016-02-24 00:58:04 +0100

    delete pagination users

--- server/actions/users/list.js
+++ server/actions/users/list.js
@@ -1,16 +1,12 @@
 module.exports = function(app){
   return function(req, res, next){
-      var skip = 0;
-
-      if(req.query['skip'])
-        skip = req.query['skip'];
 
       app.models.User.find(function(err, users){
           if(err)
               return res.status(500).send(err);
 
           res.send(users);
-      }).sort({ "title" : 1}).skip(skip).limit(10);
+      });
 
   }
 };
