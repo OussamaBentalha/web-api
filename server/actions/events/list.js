a1f4e536702ee27aea96f20eab0c163c02fbb0ff
Oussama Bentalha <oussama.bentalha@gmail.com>
2016-02-23 18:43:58 +0100

    Skip & limit

--- server/actions/events/list.js
+++ server/actions/events/list.js
@@ -3,11 +3,16 @@
  */
 module.exports = function(app){
     return function(req, res, next){
+        var skip = 0;
+
+        if(req.query['skip'])
+            skip = req.query['skip'];
+
         app.models.Event.find(function(err, events){
             if(err)
                 return res.status(500).send(err);
 
             res.send(events);
-        });
+        }).skip(skip).limit(10);
     }
 };
--- server/actions/users/list.js
+++ server/actions/users/list.js
@@ -1,10 +1,16 @@
 module.exports = function(app){
   return function(req, res, next){
+      var skip = 0;
+
+      if(req.query['skip'])
+        skip = req.query['skip'];
+
       app.models.User.find(function(err, users){
           if(err)
-            return res.status(500).send(err);
+              return res.status(500).send(err);
 
           res.send(users);
-      });
+      }).sort({ "title" : 1}).skip(skip).limit(10);
+
   }
 };
